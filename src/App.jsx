ae429d9e193f4a5f9044688dff28b65ad7f8a5a4
Rohan Taware <rohantaware.work@gmail.com>
2026-01-14 00:25:25 +0530

    added protectedroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,53 @@
-import Login from './components/Login'
-import Home from './components/Home'
-import Cart from './components/Cart'
-import Product from './components/Product'
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import Product from "./components/Product";
 import DetailedProductCard from "./components/DetailedProductCard";
-import {Routes, Route } from 'react-router-dom'
-const App=()=> {
-    return (
-      <Routes>
-        <Route index element={<Home/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/products" element={<Product />}/>
-        <Route path="/products/:id" element={<DetailedProductCard />} />
-      </Routes>
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Routes, Route } from "react-router-dom";
 
-    )
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
 
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
 
-export default App
+      <Route
+        path="/products"
+        element={
+          <ProtectedRoute>
+            <Product />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/products/:id"
+        element={
+          <ProtectedRoute>
+            <DetailedProductCard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/cart"
+        element={
+          <ProtectedRoute>
+            <Cart />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default App;
